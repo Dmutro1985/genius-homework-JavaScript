42c8b544e2637f94ed5cdb52c78f384c722f7ed0
Dmutro1985 <128517017+Dmutro1985@users.noreply.github.com>
2024-06-29 01:08:13 +0300

    Add files via upload

---
+++ js-11/index.js
@@ -0,0 +1,24 @@
+// import { name } from "./module";
+// import "./style.css";
+// import isEmail from "validator/lib/isEmail";
+// import isURL from "validator/lib/isURL";
+// import toDate from "validator/lib/toDate";
+// console.log("Your name: " + name);
+
+//console.log("Your name: " + name);
+
+
+
+// console.log(isEmail("foo@bar.com"));
+
+
+// console.log(isURL("https://www.npmjs.com/package/validator"));
+// console.log(toDate("12.12.2023"));
+// var moment = require("moment");
+// console.log(moment().format("MMM Do YY"));
+
+
+
+
+
+
